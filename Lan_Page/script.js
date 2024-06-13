6f6ce804731e4a0c06c40009c3b54b48e62ed3db
Gayatri Nikam <146618290+gayatri-nikam@users.noreply.github.com>
2024-06-13 14:25:14 +0530

    Add files via upload

---
+++ Lan_Page/script.js
@@ -0,0 +1,23 @@
+gsap.from("#img1",{
+    opacity:0,
+    delay:0.5,
+    duration:1,
+    y:60
+})
+gsap.from("#img2",{
+    opacity:0,
+    delay:0.5,
+    duration:1,
+    x:60
+})
+gsap.from("#img3",{
+    opacity:0,
+    delay:0.5,
+    duration:1,
+    y:-60 
+})
+gsap.from("#main",{
+    opacity:0,
+    delay:0.5,
+    duration:1,
+})
